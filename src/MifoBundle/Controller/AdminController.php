79fb96bfa04649e980874955ab12f5231b9fbedf
Serhii Kosyouk <monstrpro@gmail.com>
2017-12-08 23:38:31 +0200

    Add firewalls main http_basic

--- src/MifoBundle/Controller/AdminController.php
+++ src/MifoBundle/Controller/AdminController.php
@@ -31,6 +31,7 @@ class AdminController extends Controller
             $request->query->getInt('page', 1)/*page number*/,
             3/*limit per page*/
         );
+
         return $this->render('@Mifo/admin/movie/index.html.twig', array(
             'pagination' => $pagination
         ));
@@ -58,5 +59,76 @@ class AdminController extends Controller
             'movie' => $movie,
             'form' => $form->createView(),
         ));
+    }
+
+    /**
+     * Finds and displays a movie entity.
+     *
+     */
+    public function showAction(Movie $movie)
+    {
+        $deleteForm = $this->createDeleteForm($movie);
+
+        return $this->render('@Mifo/admin/movie/show.html.twig', array(
+            'movie' => $movie,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing movie entity.
+     *
+     */
+    public function editAction(Request $request, Movie $movie)
+    {
+        $deleteForm = $this->createDeleteForm($movie);
+        $editForm = $this->createForm('MifoBundle\Form\MovieType', $movie);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('movie_edit', array('id' => $movie->getId()));
+        }
+
+        return $this->render('@Mifo/admin/movie/edit.html.twig', array(
+            'movie' => $movie,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a movie entity.
+     *
+     */
+    public function deleteAction(Request $request, Movie $movie)
+    {
+        $form = $this->createDeleteForm($movie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($movie);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('movie_index');
+    }
+
+    /**
+     * Creates a form to delete a movie entity.
+     *
+     * @param Movie $movie The movie entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Movie $movie)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('movie_delete', array('id' => $movie->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+            ;
     }
 }
--- src/MifoBundle/Controller/DefaultController.php
+++ src/MifoBundle/Controller/DefaultController.php
@@ -3,11 +3,24 @@
 namespace MifoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('@Mifo/Default/index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $movies = $em->getRepository('MifoBundle:Movie')->findAll();
+        
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $movies,
+            $request->query->getInt('page', 1),
+            3
+        );
+        
+        return $this->render('@Mifo/Default/index.html.twig', [
+            'pagination' => $pagination
+        ]);
     }
 }
